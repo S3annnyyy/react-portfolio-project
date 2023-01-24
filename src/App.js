da4373749c2f3b8b7ccdf8f9936456a46f8d1d14
Sean Yap <seanyckang@gmail.com>
2023-01-24 20:15:32 +0800

    Fixed deployment issue

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />} >
         <Route index element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='project' element={<Projects />} />
-        <Route path='contact' element={<ContactPage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/project' element={<Projects />} />
+        <Route path='/contact' element={<ContactPage />} />
         </Route>
     </Routes>
     </>
